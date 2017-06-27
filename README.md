beda2ac880f01be5cf11fa875b9a9f3ef94e53d4
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2017-06-27 21:43:47 +0300

    Added Usage section to README.md

--- README.md
+++ README.md
@@ -3,3 +3,45 @@
 Machine learning functions for kotlin REPL shell.
 
 Based on deeplearning4j library.
+
+## Usage
+
+Fork this repository.
+
+Open it in IntelliJ Idea and launch Tools -> Kotlin -> Kotlin REPL (Shell)
+
+Load MNIST data set of handwritten digits by typing to REPL the following and hitting CTRL + ENTER
+
+    downloadDataSetMNIST()
+
+List your data sets:
+
+    listDataSets()
+
+Verify your data set by visualizing it as greyscale images:
+
+    visualizeDataSet("mnist.train", 28, 10)
+
+View the images under user home directory: ./kotlin-machine-learning-shell/data/images
+
+Train multi layer perceptron:
+
+    // MNIST image size
+    val rows = 28
+    val columns = 28
+    
+    // Input neuron count equals pixes in MNIST images
+    val inputCount = rows * columns
+    // Hidden neuron count could feasibly be between 300 - 1000
+    val hiddenNeuronCount = 500
+    // Output neuron count is ten as MNIST images contain hand written digits i.e. ten classes
+    val outputCount = 10 // number of output classes
+    
+    // Maximum batch size for fast GPU aided computation
+    val batchSize = 1000 // batch size for each epoch
+    val learningRate = batchSize / 1000.0 // learning rate
+    val epochCount = (10 / learningRate).toInt() // number of epochs (times entire training set is fitted to model)
+    
+    // Use multi layer perceptron.
+    val model = mlp(inputCount, hiddenNeuronCount, outputCount, learningRate)
+    train(model, "mnist.train", "mnist.test", 1000, epochCount)
--- src/test/kotlin/ml/nn/MlpTest.kt
+++ src/test/kotlin/ml/nn/MlpTest.kt
@@ -1,27 +1,31 @@
 package ml.nn
 
-import formDataSetFromTables
-import ml.shell.downloadDataSetMNIST
-import ml.shell.visualizeDataSet
-import ml.shell.visualizeTable
 import org.junit.Ignore
 import org.junit.Test
-import splitDataSetToTables
 
 internal class MlpTest {
 
     @Test
     @Ignore
-    fun testDownloadMNIST() {
-        val numRows = 28
-        val numColumns = 28
-        val inputCount = numRows * numColumns
+    fun testMlp() {
+        // MNIST image size
+        val rows = 28
+        val columns = 28
+
+        // Input neuron count equals pixes in MNIST images
+        val inputCount = rows * columns
+        // Hidden neuron count could feasibly be between 300 - 1000
+        val hiddenNeuronCount = 500
+        // Output neuron count is ten as MNIST images contain hand written digits i.e. ten classes
         val outputCount = 10 // number of output classes
+
+        // Maximum batch size for fast GPU aided computation
         val batchSize = 1000 // batch size for each epoch
         val learningRate = batchSize / 1000.0 // learning rate
-        val epochCount = (10 / learningRate).toInt() // number of epochs to perform
+        val epochCount = (10 / learningRate).toInt() // number of epochs (times entire training set is fitted to model)
 
-        val model = mlp(inputCount, 500, outputCount, learningRate)
+        // Use multi layer perceptron.
+        val model = mlp(inputCount, hiddenNeuronCount, outputCount, learningRate)
         train(model, "mnist.train", "mnist.test", 1000, epochCount)
     }
 
